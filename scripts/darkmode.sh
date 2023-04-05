bd073d646fe52c63abdf46a0fa63d7196af24793
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-04-05 12:17:01 +0200

    Update 05/04/23
    Adding pixelate on I3lock
    Divers updates

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -8,13 +8,10 @@ if [ $1 = 'l' ]; then
   # Wezterm
   sed -i 's/color_scheme = ".*"/color_scheme = "light"/' ${HOME}/.config/wezterm/wezterm.lua
   # I3
-  sed -i 's/background #21252D/background #FFFADE/' ${HOME}/.config/i3/config
-  sed -i 's/separator #FFFADE/separator #21252D/' ${HOME}/.config/i3/config
-  sed -i 's/statusline #FFFADE/statusline #21252D/' ${HOME}/.config/i3/config
-  sed -i 's/wallpaper.jpg/wallpaper-light.png/' ${HOME}/.config/i3/config
+  ln -fs ~/.config/i3/config-light ~/.config/i3/config
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
   i3-msg restart >> /dev/null
-  pkill -10 hx
+  pkill -10 helix
 
 else
 # Dark
@@ -23,11 +20,8 @@ else
   # Wezterm
   sed -i 's/color_scheme = ".*"/color_scheme = "dark"/' ${HOME}/.config/wezterm/wezterm.lua
   # I3
-  sed -i 's/background #FFFADE/background #21252D/' ${HOME}/.config/i3/config
-  sed -i 's/separator #21252D/separator #FFFADE/' ${HOME}/.config/i3/config
-  sed -i 's/statusline #21252D/statusline #FFFADE/' ${HOME}/.config/i3/config
-  sed -i 's/wallpaper-light.png/wallpaper.jpg/' ${HOME}/.config/i3/config
+  ln -fs ~/.config/i3/config-dark ~/.config/i3/config
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   i3-msg restart >> /dev/null
-  pkill -10 hx
+  pkill -10 helix
 fi 
