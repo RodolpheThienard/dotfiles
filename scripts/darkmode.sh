3b4f233abc2a96aac285601d6daa98e801e664d5
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-04-08 18:29:27 +0200

    Uptate Dark/light and Kitty

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -5,23 +5,25 @@ if [ $1 = 'l' ]; then
 # Light
   # Helix
   sed -i 's/theme = ".*"/theme = "spacebones_light"/' ${HOME}/.config/helix/config.toml
-  # Wezterm
-  sed -i 's/color_scheme = ".*"/color_scheme = "light"/' ${HOME}/.config/wezterm/wezterm.lua
+  # ALacrity
+  ln -fs ~/.config/kitty/light.conf ~/.config/kitty/theme.conf
   # I3
   ln -fs ~/.config/i3/config-light ~/.config/i3/config
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
   i3-msg restart >> /dev/null
   pkill -10 helix
+  pkill -10 kitty
 
 else
 # Dark
   # Helix
   sed -i 's/theme = ".*"/theme = "onedark"/' ${HOME}/.config/helix/config.toml
-  # Wezterm
-  sed -i 's/color_scheme = ".*"/color_scheme = "dark"/' ${HOME}/.config/wezterm/wezterm.lua
+  # ALacrity
+  ln -fs ~/.config/kitty/dark.conf ~/.config/kitty/theme.conf
   # I3
   ln -fs ~/.config/i3/config-dark ~/.config/i3/config
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   i3-msg restart >> /dev/null
   pkill -10 helix
+  pkill -10 kitty
 fi 
