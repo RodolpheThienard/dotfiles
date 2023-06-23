901e88859750fc4da49391a3ac4f02192f3a4e47
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-06-23 10:45:53 +0200

    Fresh install

---
+++ README.md
@@ -0,0 +1,9 @@
+# Configuration
+
+- Operating System : Pop Os
+- Window Manager : Gnome
+- Bar : Gnome
+- Terminal Emulator : Kitty
+- App Launcher : Gnome
+- Text Editor : Helix
+- PDF Viewer : Zathura
---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias z=zellij
+alias bat=batcat
+alias ls=exa
+alias mark3_net="ssh rodolphe@176.136.255.47"
+alias mark3="ssh rodolphe@192.168.1.6"
+alias spack-init="source /usr/share/spack/share/spack/setup-env.fish"
