88d601a267ae89955f5ff0280c767533817a0659
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-12-08 11:43:10 +0100

    Macro helix

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ end
 source ~/.config/fish/functions/config.fish
 
 #export HELIX_RUNTIME=/home/user-name/src/helix/runtime
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$HOME/Documents/zig/build/stage3/bin:$PATH"
 # export LD_LIBRARY_PATH="/usr/lib"
 
 alias l="ls -l"
@@ -28,6 +28,7 @@ alias ls=exa
 alias mark3_net="ssh rodolphe@176.136.255.47"
 alias mark3="ssh rodolphe@192.168.1.6"
 alias spack-init="source /usr/share/spack/share/spack/setup-env.fish"
-
+alias docker_compil ="docker run -it -v "$(pwd)":/compil pablooliveira/compil /bin/bash"
+alias t=trans
 set PATH $PATH /home/rodolphe/.local/bin
 
